a657c0e77a7dffb459e0c0678d4c4fa57c9b8475
Ground-Beef-1 <mjbiluvdogs@gmail.com>
2026-03-26 14:43:39 -0400

    can have an outline gradient for .dropdown-content a? I simply want the line to have gradient, or at least multiple colors

---
+++ practice/loops/loops.js
@@ -0,0 +1,25 @@
+let canvas = document.getElementById("canvas1");
+let ctx = canvas.getContext("2d");
+
+
+//fill in code for canvas1 here
+for(let y = 0; y<= 200; y += 100){
+	for(let x = 0; x<=700; x += 100){
+        	ctx.fillRect(x, y, 75, 25);
+	}
+}
+
+canvas = document.getElementById("canvas2");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas2 here
+
+canvas = document.getElementById("canvas3");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas3 here
+
+canvas = document.getElementById("canvas4");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas4 here
